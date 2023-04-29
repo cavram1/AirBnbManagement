1d0e87f5ea24e1d4eb29c302fda64f45749a840f
chant <cavram@htl-styr.ac.at>
2023-04-29 19:40:49 +0200

    github db unittest

---
+++ src/test/java/cav/airbnbmanagement/controller/CustomerRestControllerTest.java
@@ -0,0 +1,32 @@
+package cav.airbnbmanagement.controller;
+
+import cav.airbnbmanagement.model.Customer;
+import cav.airbnbmanagement.repositories.CustomerRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = {CustomerRestController.class})
+public class CustomerRestControllerTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    Customer c;
+
+    @Test
+    @Order(1)
+    public void insertintoDB() {
+        c = new Customer();
+        c.setEmail("customer@email.com");
+
+        Assertions.assertDoesNotThrow(() -> {
+            customerRepository.save(c);
+        });
+    }
+
+}
